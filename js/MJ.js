b47ff190f5e94c679daaf65c242b8c124d529b8f
unknown <itmaria.jose.pl@gmail.com>
2022-02-11 16:40:25 -0600

    agregue fotos pintura

--- js/MJ.js
+++ js/MJ.js
@@ -4,3 +4,21 @@ const contenedor = document.getElementsByClassName('contenedor');
 console.log(contenedor);
 boton.setAttribute('value','Cambio texto');
 input.setAttribute('value','Que dormidos que estan');
+
+
+function desabilita(){
+const contenedor = document.getElementById('h');
+const input = document.getElementById('cuadrito');
+var jpg = [];
+console.log(input)
+
+if(input.disabled){
+	 input.disabled = false
+	 let nuevoInput = document.getElementById("input");
+	 contenedor.appendChild(nuevoInput)
+
+}else{
+	input.disabled = true;
+}
+}
+
